5ec30c53558a78a37b1039f6bcc2ab5b145b6fd6
SANTHOSH-STARLABS <149648197+SANDY655@users.noreply.github.com>
2025-06-10 20:22:16 +0530

    minor change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,13 @@
-import tailwindcss from "@tailwindcss/vite";
-import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
-import { resolve } from "node:path";
-
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [viteReact(), tailwindcss()],
-	test: {
-		globals: true,
-		environment: "jsdom",
-	},
-	resolve: {
-		alias: {
-			"@": resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 });
